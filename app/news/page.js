13f52f88c9bc987f7e07561686a76e1b162f1e12
Alex <puskin1914@gmail.com>
2024-07-02 08:03:43 +0000

    add NewsDetailPage

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,23 @@
 import Link from 'next/link';
 
+import { DUMMY_NEWS } from '@/dummy-news';
+
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/first-news">First news</Link>
-        </li>
-        <li>
-          <Link href="/news/second-news">Second news</Link>
-        </li>
-        <li>
-          <Link href="/news/third-news">Third news</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
